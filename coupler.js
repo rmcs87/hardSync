ed50137e4b2788d83e9e89561acb2b82e42978f1
Marcello Novaes <cellonovaes@gmail.com>
2016-11-16 16:11:30 -0200

    atualizando tudo  para a versão do webmedia

--- coupler.js
+++ coupler.js
@@ -75,7 +75,7 @@ io.on('connection', function (socket) {
             //BINGO
             addScore(obj.user_id,100);
             dal.addContribution(dal.getAsset(obj.v1_url), dal.getAsset(obj.v2_url), obj.delta);
-            dal.updateAll();
+            //dal.updateAll();
             dal.print();
             done = true;
           }else{
